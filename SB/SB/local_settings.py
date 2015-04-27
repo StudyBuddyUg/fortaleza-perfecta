82f7fdc65fab988352d30b6a7f8331abfe4b63f0
lillian7 <lnassanga@gmail.com>
2015-04-27 11:48:44 +0300

    starting new project with new flashcard app

---
+++ SB/SB/local_settings.py
@@ -0,0 +1,9 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'studybuddy',
+        'USER': 'jamie',
+        'PASSWORD': '1234',
+        'HOST': ''
+    }
+}
